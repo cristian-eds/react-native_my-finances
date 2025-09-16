1abb31725543d36842c7b78a3a0e3e8c38c345d0
Cristian <cristian.santos0308@gmail.com>
2025-09-16 10:25:24 -0300

    Add GlobalStyles for authentication screens layout and styling

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container_screens_auth: {
+    flex: 1,
+    backgroundColor: '#E5EBF1',
+    paddingTop: 90,
+    paddingHorizontal: 40,
+    rowGap: 35,
+  },
+  title_screens_auth: {
+    fontSize: 36,
+    color: 'black',
+    paddingLeft: 10,
+    textAlign: 'left',
+  }
+});
