866298296fe1011e28ce87821c54475ad0e5a574
Cristian <cristian.santos0308@gmail.com>
2025-09-17 17:18:40 -0300

    Refactor user creation logic to hash passwords before storing in the database

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,21 +1,17 @@
 import { SQLiteDatabase } from "expo-sqlite";
-
-interface User {
-  id?: number;
-  name: string;
-  cpf: string;
-  password: string;
-}
-
+import { User } from "../domain/userModel";
+import bcrypt from 'react-native-bcrypt';
 
 async function create(database: SQLiteDatabase, data: User): Promise<User | undefined> {
-
-    try {
-        const statement = await database.prepareAsync(` 
+    const statement = await database.prepareAsync(` 
             INSERT INTO users (name, cpf, password)
             VALUES ($name, $cpf, $password);
         `);
-        const params = {$name: data.name, $cpf: data.cpf, $password: data.password};
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHashed = bcrypt.hashSync(data.password, salt);
+        
+        const params = {$name: data.name, $cpf: data.cpf, $password: passwordHashed};
 
         const result = await statement.executeAsync(params);
         const insertId = result.lastInsertRowId.toLocaleString(); 
@@ -24,9 +20,10 @@ async function create(database: SQLiteDatabase, data: User): Promise<User | unde
     } catch (error) {
         console.error("Error creating user:", error);
     } finally {
-        await database.closeAsync();
+        await statement.finalizeAsync();
     }
 }
+
 
 
 
