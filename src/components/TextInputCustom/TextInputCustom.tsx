11e55d97288dc8602d6a22785c57f3f254632e26
Cristian <cristian.santos0308@gmail.com>
2025-09-17 11:57:57 -0300

    Refactor TextInputCustom to accept props directly and remove unused parameters

--- src/components/TextInputCustom/TextInputCustom.tsx
+++ src/components/TextInputCustom/TextInputCustom.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput, TextInputProps, View } from 'react-native';
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { styles } from './TextInputCustomStyles';
 
 interface TextInputCustomProps {
-  placeholder: string;
   iconName?: keyof typeof MaterialIcons.glyphMap;
-  secureTextEntry?: boolean;
-  onChangeText?: (text: string) => void;
 }
 
-export function TextInputCustom({ placeholder, iconName, secureTextEntry = false, onChangeText }: TextInputCustomProps) {
+type Props = TextInputProps & TextInputCustomProps;
+
+export function TextInputCustom({ iconName, ...props}: Props) {
   return (
     <View style={styles.container}>
       {iconName && <MaterialIcons name={iconName} size={24} color="black" />}  
-      <TextInput placeholder={placeholder} secureTextEntry={secureTextEntry} style={styles.input} />
+      <TextInput {...props} style={styles.input} />
     </View>
   );
 }
