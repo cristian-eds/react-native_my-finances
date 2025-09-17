d413fc6d5c105dbbabfcf762053826a008c6e468
Cristian <cristian.santos0308@gmail.com>
2025-09-17 17:16:34 -0300

    Refactor TextInputCustom to integrate form handling with react-hook-form and display error messages

--- src/components/TextInputCustom/TextInputCustom.tsx
+++ src/components/TextInputCustom/TextInputCustom.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { TextInput, TextInputProps, View } from 'react-native';
+import { Text, TextInput, TextInputProps, View } from 'react-native';
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { styles } from './TextInputCustomStyles';
+import { FieldError, FieldPath, useController } from 'react-hook-form';
 
 interface TextInputCustomProps {
   iconName?: keyof typeof MaterialIcons.glyphMap;
+  control: any;
+  name: any;
+  errors?: FieldError | undefined;
 }
 
 type Props = TextInputProps & TextInputCustomProps;
 
-export function TextInputCustom({ iconName, ...props}: Props) {
+export function TextInputCustom({ iconName,control, name, errors, ...props}: Props) {
+
+  const {field} = useController({
+    name,
+    control,
+    defaultValue: ''
+  })
+
   return (
     <View style={styles.container}>
       {iconName && <MaterialIcons name={iconName} size={24} color="black" />}  
-      <TextInput {...props} style={styles.input} />
+      <TextInput {...props} style={styles.input} value={field.value} onChangeText={field.onChange}/>
+      {errors && <Text style={styles.error_message}>{errors.message}</Text>}
     </View>
   );
 }
