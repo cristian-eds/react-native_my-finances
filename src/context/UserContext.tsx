4fd7ed4070b7d15bb10738e231ed9ddc79162152
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-09-18 23:03:26 -0300

    Add session token verification to automatic login on contenxt

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,9 +1,10 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { User } from "../domain/userModel";
 import { useSQLiteContext } from "expo-sqlite";
 import { UserLogin } from "../domain/userLogin";
-import { login } from "../services/authService";
+import { login, loginWithSessionToken } from "../services/authService";
 import { ResponseUser } from "../domain/responseUser";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface ContentContext {
     user: Omit<User, 'password'> | null,
@@ -16,18 +17,35 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
 
     const db = useSQLiteContext();
 
-    const [user, setUser] = useState<Omit<User, 'password'> | null>(null);  
+    const [user, setUser] = useState<Omit<User, 'password'> | null>(null);
 
-    const loginUser = async (data: UserLogin) : Promise<ResponseUser | undefined> => {
+    const loginUser = async (data: UserLogin): Promise<ResponseUser | undefined> => {
         const response = await login(db, data);
-            if(response?.data) {
-              setUser(response?.data)
-            }
-            return response;
+        if (response?.data) {
+            setUser(response?.data)
+        }
+        return response;
     }
 
+    useEffect(() => {
+        console.log("Iniciou effect")
+        const verifySessionToken = async () => {
+            const token = await AsyncStorage.getItem("sessionToken");
+
+            if (!token) return;
+            
+            const response = await loginWithSessionToken(db, token);
+
+            if (response?.data) {
+                setUser(response?.data)
+            }
+        }
+
+        verifySessionToken();
+    }, [])
+
     return (
-        <UserContext.Provider value={{user, loginUser}}>
+        <UserContext.Provider value={{ user, loginUser }}>
             {children}
         </UserContext.Provider>
     );
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,7 +3,8 @@ import bcrypt from "react-native-bcrypt";
 import { ResponseUser } from "../domain/responseUser";
 import { UserLogin } from "../domain/userLogin";
 
-import * as userRepository from "../repository/userRepository"
+import * as userRepository from "../repository/userRepository";
+import { generateSessionToken } from "./sessionTokenService";
 
 export async function login(database: SQLiteDatabase, data: UserLogin): Promise<ResponseUser | undefined> {
 
@@ -14,9 +15,18 @@ export async function login(database: SQLiteDatabase, data: UserLogin): Promise<
         const isMatch = bcrypt.compareSync(data.password, hashedPassword);
 
         if (isMatch) {
+            await generateSessionToken(user, database);
             return { data: user };
         }
         return { data: null, error: "Senha incorreta" };
     }
     return { data: null, error: "Usuário não encontrado" };
 }
+
+export async function loginWithSessionToken(database: SQLiteDatabase, sessionToken: string): Promise<ResponseUser | undefined> {
+    const user = await userRepository.findUserBySessionToken(sessionToken, database);
+    if(user) {
+        return {data: user};
+    }
+    return { data: null, error: "Usuário não encontrado" };
+}   
