665ca93c47948e157ef0036c8d7433afc20c300f
Cristian <cristian.santos0308@gmail.com>
2025-09-19 09:37:55 -0300

    Add account model

---
+++ src/domain/accountModel.ts
@@ -0,0 +1,11 @@
+export interface Account {
+    id: number;
+    name: string;
+    balance: number;
+    bankCode: string;
+    type: string;
+    accountNumber: string;
+    agency: string;
+    holderName: string;
+    status: string;
+}
