0bcaacfe708eec852890133d73315565bac7a188
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-09-18 23:01:57 -0300

    Created sessionService, generateSessionToken function

---
+++ src/services/sessionTokenService.ts
@@ -0,0 +1,16 @@
+import { User } from "../domain/userModel";
+import  uuid from 'react-native-uuid';
+import * as sessionRepository from "../repository/sessionRepository";
+import { SQLiteDatabase } from "expo-sqlite";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function generateSessionToken(loggedUser: User, database: SQLiteDatabase) {
+    const sessionId = uuid.v4();
+    await sessionRepository.create(loggedUser.id.toLocaleString(),sessionId,database);
+    saveSessionTokenOnAsyncStorage(sessionId);
+}
+
+async function saveSessionTokenOnAsyncStorage(sessionToken: string){
+    AsyncStorage.setItem("sessionToken", sessionToken);
+}
+
