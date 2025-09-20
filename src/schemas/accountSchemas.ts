b92638f01a5a031d8b737f4250fa317153ca1198
Cristian <cristian.santos0308@gmail.com>
2025-09-20 10:10:05 -0300

    Refactor accountSchemas, add optional in field non required, and coerce in field balance

--- src/schemas/accountSchemas.ts
+++ src/schemas/accountSchemas.ts
@@ -5,12 +5,12 @@ import { Status } from '../domain/statusEnum';
 export const accountSchemas = z.object({
     name: z.string()
         .nonempty('O nome é obrigatório'),
-    balance: z.number("O saldo inicial é obrigatório")
+    balance: z.coerce.number("O saldo inicial é obrigatório")
         .positive("O valor deve ser positivo"),
-    bankCode: z.string(),
+    bankCode: z.string().optional(),
     type: z.enum(TypeAccount, "Tipo conta inválido"),
-    accountNumber: z.string(),
-    agency: z.string(),
-    holderName: z.string(),
-    status: z.enum(Status),
+    accountNumber: z.string().optional(),
+    agency: z.string().optional(),
+    holderName: z.string().optional(),
+    status: z.enum(Status).optional(),
 });
