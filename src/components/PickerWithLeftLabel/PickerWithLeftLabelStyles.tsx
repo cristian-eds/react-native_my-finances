6d05f16f9be4f6f6858c5e2805b8da137894c4ec
Cristian <cristian.santos0308@gmail.com>
2025-09-20 10:10:35 -0300

    Add PickerWithLeftLabel component

---
+++ src/components/PickerWithLeftLabel/PickerWithLeftLabelStyles.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  cotainer_picker: {
+    flex: 1,
+    height: 45,
+    fontSize: 16,
+    padding: 0,
+    width: 200,
+  },
+  picker: {
+    flex:1,
+    borderColor: 'white',
+    borderBottomColor: '#ccc',
+    borderRadius: 0,
+    top: -2
+  },
+  picker_text: {
+    fontSize: 14,
+    textAlign: 'left'
+  },
+   dropdownList: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingVertical: 0,
+  },
+
+});
