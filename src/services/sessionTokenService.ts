d157d71142936a90865a37e632aad52a48659a45
Cristian <cristian.santos0308@gmail.com>
2025-09-20 11:03:57 -0300

    Refactor param User to Omit<User,'pass'> on sessionTokenService

--- src/services/sessionTokenService.ts
+++ src/services/sessionTokenService.ts
@@ -4,7 +4,7 @@ import * as sessionRepository from "../repository/sessionRepository";
 import { SQLiteDatabase } from "expo-sqlite";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export async function generateSessionToken(loggedUser: User, database: SQLiteDatabase) {
+export async function generateSessionToken(loggedUser: Omit<User, "password">, database: SQLiteDatabase) {
     const sessionId = uuid.v4();
     await sessionRepository.create(loggedUser.id.toLocaleString(),sessionId,database);
     saveSessionTokenOnAsyncStorage(sessionId);
