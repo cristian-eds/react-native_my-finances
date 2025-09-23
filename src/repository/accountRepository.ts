aab5442dc796d94a18270b48f5e7e1b422b088dd
Cristian <cristian.santos0308@gmail.com>
2025-09-23 10:09:14 -0300

    Add create account on repository

--- src/repository/accountRepository.ts
+++ src/repository/accountRepository.ts
@@ -22,3 +22,30 @@ export async function findAccountByUser(userId: string, database: SQLiteDatabase
         await statement.finalizeAsync();
     }
 }
+
+export async function create(account: Omit<Account, "id">, userId: string ,database: SQLiteDatabase) : Promise<Number | undefined> {
+    const statement = await database.prepareAsync(` 
+            INSERT INTO account (name, balance, bank_code, type, account_number, agency, holder_name, status, user_id)
+            VALUES ($name, $balance, $bankCode, $type, $accountNumber, $agency, $holderName, $status, $userId);
+        `);
+
+    try {
+        const params = {$name: account.name, 
+                        $balance: account.balance, 
+                        $bankCode: account.bankCode, 
+                        $type: account.type, 
+                        $accountNumber: account.accountNumber, 
+                        $agency: account.agency, 
+                        $holderName: account.holderName, 
+                        $status: account.status, 
+                        $userId: userId};
+
+        const result = await statement.executeAsync<Account>(params);
+
+        return result.lastInsertRowId;
+    } catch (error) {
+        console.error("Error creating account:", error);
+    } finally {
+        await statement.finalizeAsync();
+    }
+}
