c5db04aa0b62fec66ab9ed7a87c3d96422b1ba7c
Cristian <cristian.santos0308@gmail.com>
2025-09-23 10:10:26 -0300

    Refactor  generateSessionToken to use Promisse.all

--- src/services/sessionTokenService.ts
+++ src/services/sessionTokenService.ts
@@ -1,16 +1,22 @@
 import { User } from "../domain/userModel";
-import  uuid from 'react-native-uuid';
+import uuid from 'react-native-uuid';
 import * as sessionRepository from "../repository/sessionRepository";
 import { SQLiteDatabase } from "expo-sqlite";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export async function generateSessionToken(loggedUser: Omit<User, "password">, database: SQLiteDatabase) {
     const sessionId = uuid.v4();
-    await sessionRepository.create(loggedUser.id.toLocaleString(),sessionId,database);
-    saveSessionTokenOnAsyncStorage(sessionId);
+    try {
+        await Promise.all([
+            sessionRepository.create(loggedUser.id.toLocaleString(), sessionId, database),
+            saveSessionTokenOnAsyncStorage(sessionId)
+        ]);
+    } catch (error) {
+        console.error("Failed to generate or save session token:", error);
+    }
 }
 
-async function saveSessionTokenOnAsyncStorage(sessionToken: string){
+function saveSessionTokenOnAsyncStorage(sessionToken: string) {
     AsyncStorage.setItem("sessionToken", sessionToken);
 }
 
