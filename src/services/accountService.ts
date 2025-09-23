be2161c5876673da01922d2533a5999439217eac
Cristian <cristian.santos0308@gmail.com>
2025-09-23 10:18:37 -0300

    Add save on accountService

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,10 +1,15 @@
 import { SQLiteDatabase } from "expo-sqlite";
 
 import * as accountRepository from '../repository/accountRepository'
+import { Account } from "../domain/accountModel";
 
 export async function getAccountByUser(userId: Number, database: SQLiteDatabase) {
     
     const account = await accountRepository.findAccountByUser(userId.toLocaleString(), database);
 
     return account;
+}
+
+export async function save(account: Omit<Account, "id">, userId: Number, database: SQLiteDatabase): Promise<Number | undefined> {
+    return await accountRepository.create(account, userId.toLocaleString(), database);
 }
