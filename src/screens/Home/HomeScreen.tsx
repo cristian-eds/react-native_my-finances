e81b25f7e3d2e58aadff3877c87338cd3aa580c2
Cristian <cristian.santos0308@gmail.com>
2025-09-24 17:59:47 -0300

    Initial structure for HomeScreem

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import { styles } from './HomeScreenStyles';
+import { styles as GlobalStyles } from '../../styles/GlobalStyles';
+
+import { CardAccount } from '../../components/CardAccount/CardAccount';
+import { ButtonPlus } from '../../components/buttons/ButtonPlus/ButtonPlus';
+import { Table } from '../../components/Table/Table';
 
 export function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
+        <View style={GlobalStyles.container_screens_normal}>
+            <CardAccount></CardAccount>
+            <View style={styles.transactions}>
+                <View style={styles.transactions_infos}>
+                    <View style={styles.transactions_infos_item}>
+                        <View style={{ flexDirection: 'row', columnGap: 8, alignItems: 'center' }}>
+                            <Text style={styles.transactions_infos_h1}>Lançamentos</Text>
+                            <MaterialIcons name="leaderboard" size={24} color="black" />
+                        </View>
+                        <ButtonPlus />
+                    </View>
+                    <View style={styles.transactions_infos_item}>
+                        <Text style={styles.transactions_infos_h2}>Período</Text>
+                        <Text style={styles.transactions_infos_h2}>Set-2025</Text>
+                    </View>
+                    <View>
+                        <View style={styles.transactions_infos_item}>
+                            <Text style={styles.transactions_infos_h3}>Créditos</Text>
+                            <Text style={styles.transactions_infos_h3}>R$ 200,00</Text>
+                        </View>
+                        <View style={styles.transactions_infos_item}>
+                            <Text style={styles.transactions_infos_h3}>Débitos</Text>
+                            <Text style={styles.transactions_infos_h3}>R$ 200,00</Text>
+                        </View>
+                        <View style={styles.transactions_infos_item}>
+                            <Text style={styles.transactions_infos_h3}>Transferência</Text>
+                            <Text style={styles.transactions_infos_h3}>R$ 200,00</Text>
+                        </View>
+                    </View>
+
+                </View>
+                <View>
+
+                </View>
+            </View>
+            <Table />
         </View>
     );
 }
--- src/screens/Home/HomeScreenStyles.tsx
+++ src/screens/Home/HomeScreenStyles.tsx
@@ -1,5 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  transactions: {
+    marginBottom: 5
+  },
+  transactions_infos: {
+    rowGap: 10
+  },
+  transactions_infos_item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  transactions_infos_h1: {
+    fontSize: 24
+  },
+  transactions_infos_h2: {
+    fontSize: 20
+  },
+  transactions_infos_h3: {
+    fontSize: 18
+  },
+  
 });
