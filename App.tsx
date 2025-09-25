9a2ad54434bbd1f5b9635837994bc77cb073fac8
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-09-24 22:54:05 -0300

    Add app backgoround color

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { Routes } from './src/routes/Routes';
 import { DatabaseProvider } from './src/database/DatabaseProvider';
 import { UserProvider } from './src/context/UserContext';
+import { View } from 'react-native';
 
 export default function App() {
   return (
     <DatabaseProvider>
       <UserProvider>
-        <Routes />
+        <View style={{backgroundColor: '#E5EBF1', flex: 1}}>
+          <Routes />
+        </View>
       </UserProvider>
     </DatabaseProvider>
   );
