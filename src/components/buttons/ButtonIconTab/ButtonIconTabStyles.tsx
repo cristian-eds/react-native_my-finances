286265abb32e8b686f0ad42764af2d9456fe4d30
Cristian <cristian.santos0308@gmail.com>
2025-09-26 10:41:48 -0300

    Create ButtonIconTab for tabs bottom

---
+++ src/components/buttons/ButtonIconTab/ButtonIconTabStyles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  icon_tab: {
+        backgroundColor: 'white', 
+        width: 100, 
+        height: 65, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        borderRadius: 15,
+        elevation: 2
+    },
+    border: {
+        borderWidth: 1,
+        borderColor: '#52525274'
+    }
+});
