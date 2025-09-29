2f309c77b57d4449ab34cabc630511be82a083a0
Cristian <cristian.santos0308@gmail.com>
2025-09-29 16:08:34 -0300

    Add update on account service

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -2,12 +2,19 @@ import { SQLiteDatabase } from "expo-sqlite";
 
 import * as accountRepository from '../repository/accountRepository'
 import { Account } from "../domain/accountModel";
+import { UpdateAccountModel } from "../domain/updateAccountModel";
+import { toAccountDomainModel } from "../mappers/accountMapper";
 
-export async function getAccountByUser(userId: Number, database: SQLiteDatabase) {
+export async function getAccountByUser(userId: Number, database: SQLiteDatabase): Promise<Account | undefined> {
     const account = await accountRepository.findAccountByUser(userId.toLocaleString(), database);
-    return account;
+    if(!account) return undefined;
+    return toAccountDomainModel(account) ;
 }
 
 export async function save(account: Omit<Account, "id">, userId: Number, database: SQLiteDatabase): Promise<Number | undefined> {
     return await accountRepository.create(account, userId.toLocaleString(), database);
+}
+
+export async function update(account: UpdateAccountModel, database: SQLiteDatabase) {
+    return await accountRepository.update(account, database);
 }
