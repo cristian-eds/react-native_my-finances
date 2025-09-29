6090382583fd1bb7a6507e0610ca5d5e99d7ec37
Cristian <cristian.santos0308@gmail.com>
2025-09-29 16:02:54 -0300

    Add prop style on RowWithLeftLabel component

--- src/components/RowWithLeftLabel/RowWithLeftLabel.tsx
+++ src/components/RowWithLeftLabel/RowWithLeftLabel.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Text, View } from 'react-native';
+import { StyleProp, Text, View, ViewStyle } from 'react-native';
 
 import { styles } from './RowWithLeftLabelStyles';
 import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
@@ -8,14 +8,15 @@ interface RowWithLeftLabelProps {
   labelText: string;
   required?: boolean;
   errors?: FieldError | Merge<FieldError, FieldErrorsImpl<{}>> |undefined;
-  children: ReactNode
-
+  children: ReactNode,
+  containerStyles?: StyleProp<ViewStyle>;
 }
 
-export function RowWithLeftLabel({labelText, required, errors, children}: RowWithLeftLabelProps) {
+
+export function RowWithLeftLabel({labelText, required, errors, containerStyles, children}: RowWithLeftLabelProps) {
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, containerStyles]}>
             <Text style={styles.text_label}>{`${labelText}${required ? "*":""}`+":"} </Text>
             {children}
             {errors && <Text style={styles.error_message}>{errors.message}</Text>}
