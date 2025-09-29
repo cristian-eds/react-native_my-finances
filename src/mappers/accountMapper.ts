1fb79091fc11ca78860947f64ac83452e780eb18
Cristian <cristian.santos0308@gmail.com>
2025-09-29 16:07:56 -0300

    Create mapper to convert account record to account model

---
+++ src/mappers/accountMapper.ts
@@ -0,0 +1,17 @@
+import { Account } from "../domain/accountModel";
+import { AccountRecord } from "../repository/models/AccountRecord";
+
+export function toAccountDomainModel(record: AccountRecord): Account {
+    return {
+        id: record.id,
+        name: record.name,
+        balance: record.balance,
+        bankCode: record.bank_code,
+        type: record.type,
+        accountNumber: record.account_number,
+        agency: record.agency,
+        holderName: record.holder_name,
+        status: record.status,
+        creationDate: record.creation_date
+    };
+}
