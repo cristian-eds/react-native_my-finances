6d6c1b20eb9b7daa4cfb6f3be16f00f8f8a0203c
Cristian <cristian.santos0308@gmail.com>
2025-10-01 17:54:28 -0300

    Initial SelectAccount component style

--- src/components/SelectAccount/SelectAccountStyles.tsx
+++ src/components/SelectAccount/SelectAccountStyles.tsx
@@ -1,12 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
   container_dropdown: {
-
-   
+    alignItems: 'center',
   },
+  dropdown: {
+    borderWidth: 0,
+  },
+  dropdown_text:{
+    textAlign: 'center',
+    fontSize: 30
+  },
+  dropDownContainerStyle: {
+    borderWidth: 0
+  },
+  labelText:{
+    fontSize: 16,
+    textAlign: 'center',
+  }
 });
