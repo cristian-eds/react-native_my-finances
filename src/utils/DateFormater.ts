6cba8de68d5f5b0ae27aefc912f55ad81d3af33a
Cristian <cristian.santos0308@gmail.com>
2025-10-03 14:17:28 -0300

    Add formater date function to return with time on utils

--- src/utils/DateFormater.ts
+++ src/utils/DateFormater.ts
@@ -6,3 +6,14 @@ export const formaterIsoDateToDefaultPattern = (data: Date): string => {
         timeZone: 'America/Sao_Paulo'
     }).format(data)
 };
+
+export const formaterIsoDateToDefaultPatternWithTime = (data: Date): string => {
+    return new Intl.DateTimeFormat('pt-BR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Sao_Paulo'
+    }).format(data)
+};
