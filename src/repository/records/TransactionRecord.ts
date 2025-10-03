2200ed5e0c39c67a46d404cc564976b97f13c51b
Cristian <cristian.santos0308@gmail.com>
2025-10-03 17:47:23 -0300

    Create transaction record model

---
+++ src/repository/records/TransactionRecord.ts
@@ -0,0 +1,12 @@
+import { MovementType } from "../../domain/enums/movementTypeEnum";
+
+export interface TransactionRecord {
+    id: number,
+    description: string,
+    value: number,
+    payment_date: string,
+    movement_type: MovementType,
+    account_id: number,
+    category_id?: number,
+    duplicate_id?: number
+}
