013649694f925335d1ab9c0ba0573758bc036422
Cristian <cristian.santos0308@gmail.com>
2025-10-03 14:17:52 -0300

    Create DateTimeInput

---
+++ src/components/DateTimeInput/DateTimeInput.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { FieldErrors, useController } from 'react-hook-form';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+import { Text, TouchableOpacity } from 'react-native';
+import { RowWithLeftLabel } from '../RowWithLeftLabel/RowWithLeftLabel';
+
+import { formaterIsoDateToDefaultPatternWithTime } from '../../utils/DateFormater';
+
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+import { styles } from './DateTimeInputSyles';
+
+interface DateTimeInputProps {
+  labelText: string,
+  name: string;
+  required?: boolean;
+  errors?: FieldErrors | undefined;
+  control: any;
+}
+
+export function DateTimeInput({ labelText, name, required, errors, control }: DateTimeInputProps) {
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const {field} = useController({
+      name,
+      control,
+    })
+
+  const showDatePicker = () => setDatePickerVisibility(true);
+  const hideDatePicker = () => setDatePickerVisibility(false);
+
+  const handleConfirm = (selectedDate: Date) => {
+    field.onChange(selectedDate)
+    hideDatePicker();
+  };
+
+  return (
+    <RowWithLeftLabel labelText={labelText} required={required}>
+      <TouchableOpacity onPress={showDatePicker} style={styles.input}>
+        <Text style={styles.text_input}>{formaterIsoDateToDefaultPatternWithTime(new Date(field.value))}</Text>
+        <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="datetime"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        is24Hour={true}
+        
+      />
+    </RowWithLeftLabel>
+  );
+}
