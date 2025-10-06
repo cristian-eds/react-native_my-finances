1641789b56f282e6d66e65ef1fbff4e61a2a1ede
Cristian <cristian.santos0308@gmail.com>
2025-10-06 15:49:51 -0300

    Refactor tab bar

--- src/routes/TabBottomNavigation.routes.tsx
+++ src/routes/TabBottomNavigation.routes.tsx
@@ -1,11 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "../screens/Home/HomeScreen";
-import { Button, Text} from "react-native";
+import { Button } from "react-native";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { Ionicons } from "@expo/vector-icons";
 
-import { styles } from './TabBottomNavigationStyles';
-import { ButtonIconTab } from "../components/buttons/ButtonIconTab/ButtonIconTab";
+import {styles} from './TabBottomNavigationStyles'
 
 const Tab = createBottomTabNavigator();
 
@@ -13,52 +13,41 @@ export function TabBottomNavigationRoutes() {
 
   const context = useContext(UserContext);
 
-  const renderTextLabel = (focused: boolean, title: string) => {
-    return (<Text style={[styles.label, focused && {fontSize: 12}]}>{title}</Text>)
-  }
-
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerStyle: { borderBottomLeftRadius: 20, borderBottomRightRadius: 20 },
         headerTitleAlign: 'center',
         headerRight: () => <Button onPress={context?.logout} title="Logout"></Button>,
-        tabBarStyle: { 
-          borderTopLeftRadius: 20, 
-          borderTopRightRadius: 20, 
-          height: 102,
-          width: "100%",
-          flexDirection: 'row',
-          justifyContent: 'center',
-          gap: 0
-        },
-      }}
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'grey',
+        tabBarStyle: styles.tabBar,
+        tabBarIcon: ({ focused, color }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+          if(route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Financas') {
+            iconName = focused ? 'cash' : 'cash-outline';
+          } else if (route.name === 'Lancamentos') {
+            iconName = 'swap-horizontal';
+          }
+          return <Ionicons name={iconName} size={24} color={color}/>
+        }
+       
+      })}
     >
       <Tab.Screen
-        name="Lançamentos"
+        name="Lancamentos"
         component={HomeScreen}
-        options={{
-          tabBarIcon: ({ focused }) => <ButtonIconTab focused={focused} iconName="compare-arrows" />,
-          tabBarLabel: ({ focused }) => renderTextLabel(focused, "Lançamentos"),
-        }}
       />
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{
-          tabBarIcon: ({ focused }) => <ButtonIconTab focused={focused} iconName="home"/>,
-          tabBarLabel: ({ focused }) => renderTextLabel(focused, "Home"),
-          title: ""
-        }}
       />
       <Tab.Screen
-        name="Finanças"
+        name="Financas"
         component={HomeScreen}
-        options={{
-          tabBarIcon: ({ focused }) => <ButtonIconTab focused={focused} iconName="attach-money"/>,
-          tabBarLabel: ({ focused }) => renderTextLabel(focused, "Finanças"),
-        }}
       />
     </Tab.Navigator>
   );
--- src/routes/TabBottomNavigationStyles.tsx
+++ src/routes/TabBottomNavigationStyles.tsx
@@ -1,11 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    label: {
-        color: 'black', 
-        top: -10,
-        elevation: 2,
-        zIndex:3,
-        fontSize: 10
+    tabBar: {
+        borderTopWidth: 0,
+        height: 100,
+        paddingBottom: 10,
+        paddingTop: 5,
     }
 });
