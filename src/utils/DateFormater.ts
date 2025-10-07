5278a4d24861880abb7703b26e9b798c2602734b
Cristian <cristian.santos0308@gmail.com>
2025-10-07 17:13:31 -0300

    Add functions on Date formater

--- src/utils/DateFormater.ts
+++ src/utils/DateFormater.ts
@@ -1,3 +1,5 @@
+import { capitalizeWord } from "./StringFormater";
+
 export const formaterIsoDateToDefaultPattern = (data: Date): string => {
     return new Intl.DateTimeFormat('pt-BR', {
         year: 'numeric',
@@ -18,9 +20,39 @@ export const formaterIsoDateToDefaultPatternWithTime = (data: Date): string => {
     }).format(data)
 };
 
-export const formaterToday = (date: Date) => {
+export const getPatterDateDayMonthDigit = (date: Date = new Date()) => {
     return Intl.DateTimeFormat('pt-BR', {
         day: '2-digit',
-        month: 'long'
-    })
+        month: 'numeric'
+    }).format(date);
 }
+
+export const getDatePatternMonthShortYearDigit = (date: Date = new Date()) => {
+    const month = Intl.DateTimeFormat('pt-BR', {
+        month: 'short'
+    }).format(date);
+
+    const year = date.getFullYear();
+
+    return `${capitalizeWord(month)}/${year}`
+}
+
+export const getWeekBounds = (date: Date = new Date()) => {
+  const currentDate = new Date(date); 
+
+  const dayOfWeek = currentDate.getDay(); 
+
+  const firstDay = new Date(currentDate);
+  firstDay.setDate(currentDate.getDate() - dayOfWeek);
+  firstDay.setHours(0, 0, 0, 0);
+
+  const lastDay = new Date(currentDate);
+  lastDay.setDate(currentDate.getDate() + (6 - dayOfWeek));
+
+  lastDay.setHours(23, 59, 59, 999);
+
+  return {
+    firstDay,
+    lastDay,
+  };
+};
