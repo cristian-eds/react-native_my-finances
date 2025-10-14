b8bc46d64371f5bdb314c314f0c1aceb6ae7050b
Cristian <cristian.santos0308@gmail.com>
2025-10-14 16:28:05 -0300

    Create IconOptions on utils

---
+++ src/utils/IconOptions.tsx
@@ -0,0 +1,84 @@
+import { Ionicons } from '@expo/vector-icons';
+
+export const iconsOptions = [
+    {
+        label: "Viajem",
+        value: "airplane-outline",
+        icon: () => <Ionicons name="airplane-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Transporte",
+        value: "bus-outline",
+        icon: () => <Ionicons name="bus-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Carro",
+        value: "cart-outline-outline",
+        icon: () => <Ionicons name="car-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Compras",
+        value: "cart-outline",
+        icon: () => <Ionicons name="cart-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Comida",
+        value: "fast-food-outline",
+        icon: () => <Ionicons name="fast-food-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Games",
+        value: "game-controller-outline",
+        icon: () => <Ionicons name="game-controller-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Lazer",
+        value: "cafe-outline",
+        icon: () => <Ionicons name="cafe-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Saude",
+        value: "medkit-outline",
+        icon: () => <Ionicons name="medkit-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Educacao",
+        value: "school-outline",
+        icon: () => <Ionicons name="school-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Contas Gerais",
+        value: "receipt-outline",
+        icon: () => <Ionicons name="receipt-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Roupas",
+        value: "shirt-outline",
+        icon: () => <Ionicons name="shirt-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Salário",
+        value: "business-outline",
+        icon: () => <Ionicons name="business-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Presente",
+        value: "gift-outline",
+        icon: () => <Ionicons name="gift-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Investimento",
+        value: "trending-up-outline",
+        icon: () => <Ionicons name="trending-up-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Venda",
+        value: "pricetag-outline",
+        icon: () => <Ionicons name="pricetag-outline" size = { 20} color="#555" />,
+    },
+    {
+        label: "Dinheiro",
+        value: "cash-outline",
+        icon: () => <Ionicons name="cash-outline" size = { 20} color="#555" />,
+    },
+]
