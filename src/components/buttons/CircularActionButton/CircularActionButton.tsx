43e38857e3f1e4a137842068292c8e46bd500a34
Cristian <cristian.santos0308@gmail.com>
2025-10-14 14:08:20 -0300

    Create CircularActionButton component

---
+++ src/components/buttons/CircularActionButton/CircularActionButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { styles } from './CircularActionButtonStyles';
+
+export function CircularActionButton({...props}: TouchableOpacityProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...props}>
+        <Ionicons name="add" size={30} color="white" />
+    </TouchableOpacity>
+  );
+}
---
+++ src/components/buttons/CircularActionButton/CircularActionButtonStyles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    padding: 10,
+    backgroundColor: '#00d111ff',
+    width: 65,
+    height: 65,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: '10%',
+    right: '10%',
+    elevation: 2
+  }
+});
