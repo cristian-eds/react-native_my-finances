78305a4814127dea07f8f6049be142221a78cc45
Cristian <cristian.santos0308@gmail.com>
2025-10-14 10:40:03 -0300

    Create SearchInput component

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInput, TextInputProps, View } from 'react-native';
+
+import { styles } from './SearchInputStyles';
+import { Ionicons } from '@expo/vector-icons';
+
+type Props = TextInputProps
+
+export function SearchInput({...props} : TextInputProps) {
+    return (
+        <View style={styles.search}>
+            <Ionicons name="search-outline" size={24} color="black" />
+            <TextInput {...props} style={styles.searchInput} />
+        </View>
+    );
+}
---
+++ src/components/SearchInput/SearchInputStyles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    search: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        paddingHorizontal: 15,
+        paddingVertical: 3,
+        borderRadius: 15
+    },
+    searchInput: {
+        flex: 1
+    }
+});
