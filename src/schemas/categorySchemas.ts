f551b2df9497f7df26eda102156d97ab98ac7721
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-10-14 23:15:49 -0300

    Adjust iconName, hexColor field values validation on schemas

--- src/schemas/categorySchemas.ts
+++ src/schemas/categorySchemas.ts
@@ -1,13 +1,15 @@
 import z from "zod";
 import { MovementType } from "../domain/enums/movementTypeEnum";
+import { iconsOptions } from "../utils/IconOptions";
+import { hexColorOptions } from "../utils/HexColorOptions";
+
+const iconValues = iconsOptions.map(item => item.value);
+const hexColorValues = hexColorOptions.map(item => item.value);
 
 export const categorySchemas = z.object({
     description: z.string()
         .nonempty('A descrição é obrigatória.'),
     movementType: z.enum(MovementType, "Tipo movimento inválido"),
-    iconName: z.string()
-        .nonempty('O icone é obrigatório.'),
-    hexColor: z.string()
-        .nonempty('A cor é obrigatória.'),
+    iconName: z.enum(iconValues, 'Nome icone inválido'),
+    hexColor: z.enum(hexColorValues, 'Cor inválida'),
 })
-    ;
