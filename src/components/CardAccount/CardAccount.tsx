c4e2d69e17b80755551ac3df33246e0e73b1d82f
Cristian <cristian.santos0308@gmail.com>
2025-10-23 09:47:32 -0300

    Changed CardAccount layout and styles

--- src/components/CardAccount/CardAccount.tsx
+++ src/components/CardAccount/CardAccount.tsx
@@ -30,16 +30,14 @@ export function CardAccount() {
 
     return (
         <TouchableOpacity style={styles.card} onPress={handleNavigateToAccountDetails}>
-            <Text style={{ fontSize: 14, textAlign: 'center' }}>Conta</Text>
             <View style={styles.card_header}>
-                <Ionicons name="key-outline" size={20} color="black" style={{ flex: 1 }} />
-                <View style={{ alignItems: 'center', flex: 4 }}>
-                    <SelectAccount />
+                <View style={{flex: 2 }}>
+                    <SelectAccount labelStyle={{fontSize: 35, textAlign: 'left'}}/>
                 </View>
                 <Ionicons name="add" size={25} color="black" style={{ flex: 1, textAlign: 'right' }} onPress={() => setShowModalAddAccount(true)} />
             </View>
             <View>
-                <Text style={{ fontSize: 20 }}>Saldo:</Text>
+                <Text style={styles.cardText}>Saldo Disponível:</Text>
                 <View style={styles.card_info}>
                     <Text style={styles.card_info_text_balance}>{formaterNumberToBRL(activeAccount?.balance)}</Text>
                     <Ionicons name="eye-outline" size={24} color="black" />
--- src/components/CardAccount/CardAccountStyles.tsx
+++ src/components/CardAccount/CardAccountStyles.tsx
@@ -2,13 +2,18 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   card: {
-    borderWidth: 1,
-    borderColor: '#b2b2b2ff',
     borderRadius: 20,
-    paddingVertical:8,
-    paddingHorizontal: 20,
+    paddingVertical: 10,
+    paddingRight: 30,
+    paddingLeft: 15,
     backgroundColor: 'white',
     zIndex: 5,
+    rowGap: 15,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5
   },
   card_header: {
     flexDirection: 'row',
@@ -22,6 +27,13 @@ export const styles = StyleSheet.create({
   },
   card_info_text_balance: {
     fontSize: 32,
-    fontWeight: '400'
+    fontWeight: '500',
+    paddingLeft: 15,
+  },
+  cardText: {
+    fontSize: 14,
+    color: '#747474cf',
+    textAlign: 'left',
+    paddingLeft: 13
   }
 });
