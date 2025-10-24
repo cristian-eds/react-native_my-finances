129f686af7fc323166654ce197d8ba04a80ac0fa
Cristian <cristian.santos0308@gmail.com>
2025-10-24 10:45:07 -0300

    Add functions to get color and text in MovementType enum

--- src/domain/enums/movementTypeEnum.ts
+++ src/domain/enums/movementTypeEnum.ts
@@ -3,3 +3,27 @@ export enum MovementType {
     Despesa = 'DESPESA',
     Transferencia = 'TRANSFERENCIA'
 }
+
+export const frontColorByMovementType = (movementType: MovementType) => {
+    switch (movementType) {
+        case MovementType.Despesa:
+            return 'rgba(222, 29, 29, 0.89)'
+        case MovementType.Receita:
+            return 'rgba(14, 142, 14, 0.89)'
+        default:
+            return 'rgba(24, 68, 163, 0.89)'
+    }
+}
+
+export const textMovementType = (movementType: MovementType | null) => {
+    switch (movementType) {
+        case MovementType.Despesa:
+            return 'Débitos'
+        case MovementType.Receita:
+            return 'Créditos'
+        case MovementType.Transferencia:
+            return 'Transferências'
+        default:
+            return 'Geral'
+    }
+}
