e5d62debc22a6e44116250d38b66cf6f89af3245
Cristian <cristian.santos0308@gmail.com>
2025-10-24 10:44:42 -0300

    Add styles on Row structure component props

--- src/components/modals/structure/Row/Row.tsx
+++ src/components/modals/structure/Row/Row.tsx
@@ -1,10 +1,13 @@
 import React, { PropsWithChildren } from 'react';
-import { View } from 'react-native';
+import { View, ViewProps, ViewStyle } from 'react-native';
 
+interface RowProps {
+  style?: ViewStyle
+}
 
-export function Row({children}:PropsWithChildren) {
+export function Row({style, children}:PropsWithChildren<RowProps>) {
   return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', columnGap: 10, alignItems: 'center'}}>
+    <View style={[{flexDirection: 'row', justifyContent: 'space-between', columnGap: 10, alignItems: 'center'}, style]}>
         {children}
     </View>
   );
