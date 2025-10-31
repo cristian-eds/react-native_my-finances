783f2b61fdc548ae691d46e0002f538fc254a852
Cristian <cristian.santos0308@gmail.com>
2025-10-31 09:58:19 -0300

    Fix: schemas type in filtersSchemas

--- src/utils/schemas/filtersSchemas.ts
+++ src/utils/schemas/filtersSchemas.ts
@@ -3,6 +3,6 @@ import { MovementType } from "../../domain/enums/movementTypeEnum";
 
 export const filtersSchemas = z.object({
     movementType: z.array(z.enum(MovementType, "Tipo movimento inválido")).optional(),
-    categories: z.array(z.string()).optional(),
-    accounts: z.array(z.string()).optional(),
+    categories: z.array(z.number()).optional(),
+    accounts: z.array(z.number()).optional(),
 });
