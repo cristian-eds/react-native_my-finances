c9f8645fde445cd8bd783494c2e2321391f8d85d
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-11-03 22:41:08 -0300

    Add ordernation on schemas

--- src/utils/schemas/filtersSchemas.ts
+++ src/utils/schemas/filtersSchemas.ts
@@ -1,8 +1,12 @@
 import z from "zod";
 import { MovementType } from "../../domain/enums/movementTypeEnum";
+import { ColumnsOrderTransaction } from "../../domain/enums/columnsOrderTransaction";
+import { OrderTypes } from "../../domain/enums/orderTypes";
 
 export const filtersSchemas = z.object({
     movementType: z.array(z.enum(MovementType, "Tipo movimento inválido")).optional(),
     categories: z.array(z.number()).optional(),
     accounts: z.array(z.number()).optional(),
+    orderType: z.enum(OrderTypes).optional(),
+    orderColumn: z.enum(ColumnsOrderTransaction).optional()
 });
