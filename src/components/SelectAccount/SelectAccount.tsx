fcc26d98800b3552a10aa84a0fdb6f53312b7a4b
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-11-04 22:19:05 -0300

    Add general account option on SelectAccount component

--- src/components/SelectAccount/SelectAccount.tsx
+++ src/components/SelectAccount/SelectAccount.tsx
@@ -5,6 +5,9 @@ import { useAccountStore } from '../../stores/AccountStore';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 import { styles } from './SelectAccountStyles';
+import { mapAccountsToItemsDropdown } from '../../utils/mappers/itemsPickerMapper';
+import { Status } from '../../domain/enums/statusEnum';
+import { TypeAccount } from '../../domain/enums/typeAccountEnum';
 
 interface SelectAccountProps {
     containerStyle?: StyleProp<ViewStyle>;
@@ -16,13 +19,28 @@ export function SelectAccount({ containerStyle, labelStyle }: SelectAccountProps
     const [open, setOpen] = useState(false);
     const { accounts, activeAccount, setActiveAccount } = useAccountStore();
 
-    const itemsToDropDown = accounts.map((account) => { return { label: account.name, value: account.id } })
+    const itemsToDropDown = [...mapAccountsToItemsDropdown(accounts), {label: 'Geral', value: 0}]
 
     const handleSetNewAccount = (accountId: number) => {
+        if(accountId === 0) {
+            setActiveAccount({
+                status: Status.Ativo,
+                accountNumber: '9999',
+                agency:'9999',
+                balance: accounts.reduce((prev, current) => current.balance + prev, 0),
+                bankCode: '999',
+                creationDate: new Date().toISOString(),
+                holderName: 'ADMIN',
+                id: 0,
+                name: 'Geral',
+                type: TypeAccount.Corrente
+            })
+            return;
+        }
         const newActiveAccount = accounts.find(acc => acc.id === accountId);
         if (newActiveAccount) {
             setActiveAccount(newActiveAccount);
-        }
+        } 
     }
 
     return (
