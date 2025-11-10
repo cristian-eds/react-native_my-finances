4a18610672d810736ac97f936cc1d1c4ff7ee00f
Cristian <cristian.santos0308@gmail.com>
2025-11-10 11:37:15 -0300

    Create DuplicateRecord

---
+++ src/repository/records/DuplicateRecord.ts
@@ -0,0 +1,12 @@
+import { MovementType } from "../../domain/enums/movementTypeEnum";
+
+export interface DuplicateRecord {
+    id: number,
+    description: string,
+    total_value: number,
+    issue_date: Date,
+    due_date: Date,
+    movement_type: MovementType,
+    account_id: number,
+    category_id: number
+}
