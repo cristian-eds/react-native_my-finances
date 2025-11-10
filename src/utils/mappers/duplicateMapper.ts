1ee2daf89e4e6b4377dadfbe8069e333b9856224
Cristian <cristian.santos0308@gmail.com>
2025-11-10 11:45:40 -0300

    Create DuplicateMapper

---
+++ src/utils/mappers/duplicateMapper.ts
@@ -0,0 +1,19 @@
+import { DuplicateModel } from "../../domain/duplicateModel";
+import { DuplicateRecord } from "../../repository/records/DuplicateRecord";
+
+export function fromRecordToModel(record: DuplicateRecord) : DuplicateModel{
+    return {
+        accountId: record.account_id,
+        categoryId: record.category_id,
+        description: record.description,
+        dueDate: record.due_date,
+        id: record.id,
+        issueDate: record.issue_date,
+        movementType: record.movement_type,
+        totalValue: record.total_value
+    }
+}
+
+export function fromRecordListToModelList(records: DuplicateRecord[]) : DuplicateModel[] {
+    return records.map(fromRecordToModel);
+}
