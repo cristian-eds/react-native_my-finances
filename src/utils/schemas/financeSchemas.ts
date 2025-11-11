ce6c63cd69bbdfc8805a2a29c8c6ffd3c847ffc0
Cristian <cristian.santos0308@gmail.com>
2025-11-11 10:51:07 -0300

    Create financeSchemas

---
+++ src/utils/schemas/financeSchemas.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+import { MovementType } from "../../domain/enums/movementTypeEnum";
+
+export const financeSchemas = z.object({
+    issueDate: z.coerce.date('Data emissão inválida'),
+    dueDate: z.coerce.date('Data vencimento inválida'),
+    totalValue: z.coerce.number("O valor é obrigatório"),
+    description: z.string()
+        .nonempty('A descrição é obrigatória.'),
+    movementType: z.enum(MovementType, "Tipo movimento inválido"),
+    categoryId: z.string('Categoria inválida!').optional(),
+    accountId: z.string('A conta para transação é obrigatória'),
+})
