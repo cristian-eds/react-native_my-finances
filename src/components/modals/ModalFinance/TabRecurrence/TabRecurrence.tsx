d054b134552f0e769a652885af369cdf6dad29ce
Cristian <cristian.santos0308@gmail.com>
2025-11-28 15:57:03 -0300

    TabRecurrence extracted for component

---
+++ src/components/modals/ModalFinance/TabRecurrence/TabRecurrence.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { styles } from './TabRecurrenceStyles';
+import { TextInputWithTopLabel } from '../../../TextInputWithTopLabel/TextInputWithTopLabel';
+import { TypeRecurrence } from '../../../../domain/enums/typeRecurrence';
+import { RowWithTopLabel } from '../../../RowWithTopLabel/RowWithTopLabel';
+import { Checkbox } from '../../../Checkbox/Checkbox';
+import { TouchableOpacity } from 'react-native';
+import { ModalInstallments } from '../../ModalInstallments/ModalInstallments';
+
+interface TabRecurrenceProps {
+    control: any;
+    errors: any;
+    activeTypeRecurrence: TypeRecurrence;
+    setActiveTypeRecurrence: (type: TypeRecurrence) => void;
+    data: any;
+}
+
+export function TabRecurrence({ control, errors, activeTypeRecurrence, setActiveTypeRecurrence, data }: TabRecurrenceProps) {
+
+    const [showModalInstallments, setShowModalInstallments] = React.useState(false);
+
+    const handleGenerateInstallments = () => {
+        let items = [];
+        if (activeTypeRecurrence === TypeRecurrence.Fixo) {
+            const fixedDate = data.fixedInstallmentDate;
+            for (let i = 0; i < data.numberInstallments; i++) {
+                items.push({
+                    sequencyItem: i + 1,
+                    dueDate: new Date(new Date(data.issueDate as Date).getFullYear(), new Date(data.issueDate as Date).getMonth() + i, fixedDate),
+                    value: Number(data.totalValue),
+                    description: data.description + ` - ${i + 1}/${data.numberInstallments}`
+                });
+            }
+        }
+        else if (activeTypeRecurrence === TypeRecurrence.Intervalo) {
+            const intervalDays = data.intervalBetweenInstallments || 30;
+            for (let i = 0; i < data.numberInstallments; i++) {
+                items.push({
+                    sequencyItem: i + 1,
+                    dueDate: new Date(new Date(data.issueDate as Date).getTime() + (i * intervalDays as number * 24 * 60 * 60 * 1000)),
+                    value: Number(data.totalValue) / data.numberInstallments,
+                    description: data.description + ` - ${i + 1}/${data.numberInstallments}`
+                });
+            }
+        }
+        return items;
+    }
+
+    const renderTypeRecurrenceItem = (title: string, subtitle: string, type: TypeRecurrence) => {
+        return (
+            <RowWithTopLabel title='Tipo da recorrência' errors={errors.typeRecurrence} stylesProp={{ justifyContent: 'flex-start', columnGap: 10 }}>
+                <Checkbox checked={type === activeTypeRecurrence} />
+                <TouchableOpacity onPress={() => setActiveTypeRecurrence(type)}>
+                    <Text>{title}</Text>
+                    <Text style={{ fontSize: 12, color: '#7b7b7bff' }}>{subtitle}</Text>
+                </TouchableOpacity>
+            </RowWithTopLabel>
+        )
+    }
+
+    return (
+        <>
+            <TextInputWithTopLabel title='Número de Parcelas' control={control} name='numberInstallments' errors={errors.numberInstallments} placeholder='Quantidade de parcelas' keyboardType='number-pad' required />
+            <View style={{ rowGap: 10, marginTop: 5 }}>
+                <Text>Tipo de Recorrência</Text>
+                {renderTypeRecurrenceItem('Dia Fixo', 'Vence no mesmo dia em todos os meses.', TypeRecurrence.Fixo)}
+                {renderTypeRecurrenceItem('Intervalo de dias', 'Número de dias entre cada parcela.', TypeRecurrence.Intervalo)}
+            </View>
+            <View style={{ rowGap: 14, marginTop: 5 }}>
+                {activeTypeRecurrence === TypeRecurrence.Fixo ?
+                    <>
+                        <Text>Dia do vencimento</Text>
+                        <TextInputWithTopLabel title='Dia do mês' control={control} name='fixedInstallmentDate' errors={errors.fixedInstallmentDate} placeholder='Informe o dia do mês' keyboardType='number-pad' required />
+                    </> :
+                    <>
+                        <Text>Dias entre parcelas</Text>
+                        <TextInputWithTopLabel title='Intervalo de dias' control={control} name='intervalBetweenInstallments' errors={errors.intervalBetweenInstallments} placeholder='Informe o intervalo de dias' keyboardType='number-pad' required />
+                    </>
+                }
+            </View>
+            <TouchableOpacity style={styles.buttonInstallmentsPreview} onPress={() => setShowModalInstallments(true)}>
+                <Ionicons name="calendar-clear-outline" size={18} color="black" />
+                <Text>Ver pré-visualização das parcelas</Text>
+            </TouchableOpacity>
+            {showModalInstallments && <ModalInstallments isShow={showModalInstallments} items={handleGenerateInstallments()} onClose={() => setShowModalInstallments(false)}/>}
+        </>
+    );
+}
