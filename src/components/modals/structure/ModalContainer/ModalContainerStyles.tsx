d6d8e6a51a85ec55c108743de4d8e607f4cf9746
Cristian <cristian.santos0308@gmail.com>
2025-12-05 14:26:01 -0300

    Adjust padding in ModalContainer

--- src/components/modals/structure/ModalContainer/ModalContainerStyles.tsx
+++ src/components/modals/structure/ModalContainer/ModalContainerStyles.tsx
@@ -3,9 +3,9 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
+        paddingTop: 100,
         backgroundColor: '#63636345'
     }
 });
