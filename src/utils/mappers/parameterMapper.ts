15eb6839ece0073d37cc66ffeb0b3edf7a6c01c5
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-12-27 14:33:21 -0300

    Create: Mapper to parameters

---
+++ src/utils/mappers/parameterMapper.ts
@@ -0,0 +1,12 @@
+import { ParameterModel } from "../../domain/paremetersModel"
+import { ParametersRecord } from "../../repository/records/ParametersRecord"
+
+
+export const toParameterModel = (record: ParametersRecord): ParameterModel => {
+    return {
+        userId: record.user_id,
+        enableTransactionNotify: record.enable_transaction_notify === 1,
+        enableDuplicateNotify: record.enable_duplicate_notify === 1,
+        duplicateNotificationTime: new Date(`1970-01-01T${record.duplicate_notification_time}:00`)
+    }
+}
