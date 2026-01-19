4af5734d267df7b028690e7247c93c7d5409867c
Cristian <cristian.santos0308@gmail.com>
2026-01-19 16:21:59 -0300

    Add: Time to ability to confirm in ModalConfirm

--- src/components/modals/ModalConfirm/ModalConfirm.tsx
+++ src/components/modals/ModalConfirm/ModalConfirm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { styles } from './ModalConfirmStyles';
 import { Modal, View } from 'react-native';
@@ -11,9 +11,23 @@ interface ModalConfirmProps {
     onConfirm?: () => void;
     title: string,
     text?: string,
+    secondsToAbilityConfirm?: number
 }
 
-export function ModalConfirm({ isShow, onClose, onConfirm, title, text }: ModalConfirmProps) {
+export function ModalConfirm({ isShow, onClose, onConfirm, title, text, secondsToAbilityConfirm }: ModalConfirmProps) {
+
+    const [mode, setMode] = useState<Mode>(secondsToAbilityConfirm ? Mode.DISABLED : Mode.CONFIRM);
+
+    useEffect(() => {
+        if(!secondsToAbilityConfirm) return;
+
+        const timer = setTimeout(() => { 
+            setMode(Mode.CONFIRM)
+        }, secondsToAbilityConfirm);
+
+        return () => clearTimeout(timer);
+    }, [secondsToAbilityConfirm])
+
 
     return (
         <Modal
@@ -25,10 +39,10 @@ export function ModalConfirm({ isShow, onClose, onConfirm, title, text }: ModalC
             <View style={styles.container}>
                 <View style={styles.container_content}>
                     <Text style={styles.title}>{title}</Text>
-                    {text && <Text style={styles.text}>{text}</Text> }
+                    {text && <Text style={styles.text}>{text}</Text>}
                     <View style={styles.buttons}>
-                        <ButtonIconAction iconName='close' onPress={onClose}/>
-                        <ButtonIconAction onPress={onConfirm}  mode={Mode.CONFIRM} />
+                        <ButtonIconAction iconName='close' onPress={onClose} />
+                        <ButtonIconAction onPress={onConfirm} mode={mode} />
                     </View>
                 </View>
 
